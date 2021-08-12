4ca3d42b49fba440b1e2c87b61d86689a0f90620
Stacy David Thurston <tigerfarm@gmail.com>
2021-08-12 16:58:59 -0700

    update

--- HttpPost.js
+++ HttpPost.js
@@ -4,8 +4,8 @@ var basicAuth = "Basic " + Buffer.from("dave" + ":" + "password").toString("base
 // To echo the form data, the URI needs to have a value such as "/show".
 
 // var theRequestUrl = "http://localhost:3000/2010-04-01/Accounts/123456/Messages." + "json";
-var theRequestUrl = 'http://localhost:3000/post';
-// var theRequestUrl = 'https://tfpecho.herokuapp.com/post';
+// var theRequestUrl = 'http://localhost:3000/post';
+var theRequestUrl = 'https://tfpecho.herokuapp.com/post';
 var theFormData = {
     From: "+16505551111",
     To: "+16505552222",
